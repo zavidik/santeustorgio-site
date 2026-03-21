e340ee75bd561523432c7ea1ec6ed619a4f530e3
GiorgiaZanella <zangio2000@gmail.com>
2026-03-21 10:46:45 +0100

    Minor update

--- js/i18n.js
+++ js/i18n.js
@@ -54,7 +54,7 @@ const translations = {
     footer_address: "Piazza Sant'Eustorgio, 6, 20122 Milano MI",
     footer_phone: "+39 02 0000000",
     footer_email: "ristorante@santeustorgiomilano.it",
-    footer_instagram: "ristorante_sant'eustorgiomilano",
+    footer_instagram: " ",
     footer_rights: "© 2025 Ristorante Sant'Eustorgio. Tutti i diritti riservati.",
 
     // BOOKING MODAL
