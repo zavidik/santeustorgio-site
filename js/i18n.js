b3e179b70bc256180aef028adc7ae078c95d043c
GiorgiaZanella <zangio2000@gmail.com>
2026-03-22 13:19:58 +0100

    contatti page

--- js/i18n.js
+++ js/i18n.js
@@ -243,7 +243,7 @@ const translations = {
     contatti_email_title: "Email",
     contatti_email: "ristorante@santeustorgiomilano.it",
     contatti_instagram_title: "Instagram",
-    contatti_instagram: "@ristorante_sant_eustorgiomilano",
+    contatti_instagram: "@ristorante_santeustorgiomilano",
     contatti_map_title: "Scopri dove siamo",
     contatti_map_open: "Apri in Google Maps",
     contatti_hours_title: "Orari",
